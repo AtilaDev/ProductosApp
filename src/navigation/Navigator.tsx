a521ea2051670c4d33f95019afe6f6af44a8f6f6
Leandro <atiladevelop@gmail.com>
2021-10-25 00:09:04 -0300

    React Navigation configured

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import ProtectedScreen from '../screens/ProtectedScreen';
 
 const Stack = createStackNavigator();
 
-function HomeScreen() {
-  return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
 export const Navigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#fff',
+        },
+      }}>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
     </Stack.Navigator>
   );
 };
