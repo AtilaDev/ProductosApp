07d7f869efd698b71510a3cb2446bab839365161
Leandro <atiladevelop@gmail.com>
2021-10-27 20:46:12 -0300

    [WIP]: Working on Form and Navigation

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 import Background from '../components/Background';
 import WhiteLogo from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
@@ -10,16 +16,59 @@ export default function LoginScreen() {
       {/* Background */}
       <Background />
 
-      {/* Keyboard avoid view */}
-      <WhiteLogo />
-      <Text style={loginStyles.title}>Login</Text>
+      <View style={loginStyles.formContainer}>
+        {/* Keyboard avoid view */}
+        <WhiteLogo />
+        <Text style={loginStyles.title}>Login</Text>
 
-      <Text style={loginStyles.label}>Email:</Text>
-      <TextInput
-        placeholder="Email"
-        placeholderTextColor="rgba(255,255,255,.4)"
-        keyboardType="email-address"
-      />
+        <Text style={loginStyles.label}>Email:</Text>
+        <TextInput
+          placeholder="Email"
+          placeholderTextColor="rgba(255,255,255,.4)"
+          keyboardType="email-address"
+          underlineColorAndroid="#fff"
+          style={[
+            loginStyles.inputField,
+            Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="#fff"
+          // TODO: onChange, value
+
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <Text style={loginStyles.label}>Password:</Text>
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="rgba(255,255,255,.4)"
+          keyboardType="default"
+          underlineColorAndroid="#fff"
+          style={[
+            loginStyles.inputField,
+            Platform.OS === 'ios' && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="#fff"
+          // TODO: onChange, value
+
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        {/* Login Button */}
+        <View style={loginStyles.buttonContainer}>
+          <TouchableOpacity activeOpacity={0.7} style={loginStyles.button}>
+            <Text style={loginStyles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Create an account */}
+        <View style={loginStyles.newUserContainer}>
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text style={loginStyles.buttonText}>New Account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 }
